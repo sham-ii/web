f93e18ff80272e4f82e8b64564c0060b55c3ad21
shamae <sapedashamae@gmail.com>
2024-06-13 15:40:10 +0800

    Add files via upload

---
+++ js/js.js
@@ -0,0 +1,12 @@
+function inp1(){
+    document.getElementById('label1').style.color="red";
+    document.getElementById('label3').style.color="green";
+}
+function inp2(){
+    document.getElementById('label2').style.color="red";
+    document.getElementById('label3').style.color="green";
+}
+function inp3(){
+    document.getElementById('label3').style.color="green";
+}
+
